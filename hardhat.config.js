8d64bb59e3a12bfd4174f1e51da666a73d8f700d
David Biagiola <david.biagiola@posibillian.tech>
2024-09-02 17:32:38 -0400

    add ganahce network; add complete deploy script for dex SC

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    ganache: {
+      url: "http://127.0.0.1:7545",
+      accounts: [
+        `0x3131bdca747ec9ee8b0c3a9eeadb2306988ce97f32692f71d043b51aaeb96939`
+      ]
+    }
+  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,25 +4,32 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+const hre = require('hardhat');
+const fs = require('fs/promises');
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const Token = await hre.ethers.getContractFactory('Token');
+  const token = await Token.deploy('100');
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const DEX = await hre.ethers.getContractFactory('DEX');
+  const dex = await DEX.deploy(token.target, 100);
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  await token.waitForDeployment();
+  await writeDeploymentInfo(token, 'token.json');
 
-  await lock.waitForDeployment();
+  await dex.waitForDeployment();
+  await writeDeploymentInfo(dex, 'dex.json');
+}
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+async function writeDeploymentInfo(contract, filename= '') {
+  const data = {
+    network: hre.network.name,
+    contractAddress: contract.target,
+    abi: contract.interface.format()
+  }
+
+  const content = JSON.stringify(data, null, 2);
+  await fs.writeFile(filename, content, { encoding: 'utf-8' });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
